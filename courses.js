9d70ccfb8dcce2fb3ce7fc78cf5c055775f6dd0b
Raúl SanGar <rarosagar.igf@gmail.com>
2020-10-04 02:13:17 -0500

    Students detail

--- courses.js
+++ courses.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     messagingSenderId: "477113114096",
     appId: "1:477113114096:web:292e0794332bd76b6cd135",
     measurementId: "G-2LG9BXE7LL"
-  };
+};
 
 firebase.initializeApp(firebaseConfig);
 //Database start
@@ -24,6 +24,7 @@ const authorInput = document.querySelector('#authorCourse')
 const dateInput = document.querySelector('#dateCourse')
 const durationInput = document.querySelector('#durationCourse')
 const messagesContainer = document.querySelector('#listBefore')
+const textContainer = document.querySelector('#textContainer')
 
 //const googleButton = document.querySelector('#loginWithGoogle')
 //const logoutButton = document.querySelector('#logout')
@@ -31,28 +32,28 @@ const messagesContainer = document.querySelector('#listBefore')
 const imageInput = document.querySelector('#imageInput')
 
 
-function uploadToStorage(file, docId) { 
+function uploadToStorage(file, docId) {
     return new Promise(function (resolve, reject) {
         const imageRef = storage.ref(`courses/images/${docId}.${file.type.split('/')[1]}`)
         imageRef.put(file)
-            .then(()=>{
+            .then(() => {
                 imageRef.getDownloadURL()
-                    .then(function(url){
+                    .then(function (url) {
                         resolve(url)
                     })
-                    .catch((error)=>{
+                    .catch((error) => {
                         console.log(error)
                         reject(error)
                     })
             })
-            .catch((error)=>{
+            .catch((error) => {
                 console.log(error)
                 reject(error)
             })
     })
 }
 
-sendButton.addEventListener('click', (event)=>{
+sendButton.addEventListener('click', (event) => {
     event.preventDefault()
     const image = imageInput.files[0]
     db.collection('courses').add({
@@ -62,67 +63,88 @@ sendButton.addEventListener('click', (event)=>{
         duration: durationInput.value,
         startDate: dateInput.value,
         timestamp: firebase.firestore.Timestamp.now()
-    }).then(function(docRef){
+    }).then(function (docRef) {
         uploadToStorage(image, docRef.id)
-            .then(function(url) {
+            .then(function (url) {
                 db.collection('courses').doc(docRef.id).update({
-                    image:url
-                }).then(function() {
+                    image: url
+                }).then(function () {
                     console.log('Exitoso')
-                }).catch(function(error){
+                }).catch(function (error) {
                     console.log('No exitoso')
                 })
             })
-    }).catch(function(error){
+    }).catch(function (error) {
         console.log(error)
         alert('No se pudo guardar el curso')
     })
     messagesContainer.innerHTML = ''
+    buttonDetail.innerHTML = ''
+
 })
 
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
     db.collection('courses')
-    .orderBy("timestamp","asc")
-    .onSnapshot(function(querySnapshot) {
-        messagesContainer.innerHTML = ''
-        const courses = []
-        querySnapshot.forEach(function(item){
-            courses.push(item.data())
-        })
-        let innerHTML = '<article>'
-        courses.forEach(function(course) {
-            innerHTML += `
-                <button class="collapsable"> ${course.title} </button>
-                <div class="collapse"> ${course.description} </div>
-                <div class="collapse"> ${course.startDate} </div>
-                <div class="collapse"> ${course.duration} </div>
-                <div class="collapse"> ${course.teacher} </div>
-                ${course.image ?
-                    `<img src="${course.image}" alt="image" width="50" height="50">` 
-                    : '<div class="collapse">imagen no disponible</div>'
-                }
-        
-            `
+        .orderBy("timestamp", "asc")
+        .onSnapshot(function (querySnapshot) {
+            messagesContainer.innerHTML = ''
+
+            const courses = []
+            querySnapshot.forEach(function (item) {
+                courses.push({'id':item.id, ...item.data()})
+                console.log(item)
+            })
+            let innerHTML = ''
+
+            courses.forEach(function (course) {
+                
+                btn = courses.map(course => `<button id="${course.id}" onclick="functional(this)"> ${course.title} </button>`)
+                messagesContainer.innerHTML = btn.join(' ')
+                console.log(course.id)
+
+            
+                innerHTML += `
+              
+              
+                `
+
+
+            })
+            innerHTML += ''
+
+            messagesContainer.innerHTML += innerHTML;
 
         })
-        innerHTML += '</article>'
-        messagesContainer.innerHTML += innerHTML;
-    })
+
 })
 
 
-let coll = document.querySelectorAll('.collapsable');
 
-
-for (let i = 0; i < coll.length; i++) {
-    console.log(coll)
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var collapse = this.nextElementSibling;
-        if (collapse.style.display === "block") {
-            collapse.style.display = "none";
-        } else {
-            collapse.style.display = "block";
+function functional(id) {
+    db.collection('courses').doc(id.id).get().then(doc => {
+        
+        if (doc.exists){
+            textContainer.innerHTML = `<p> Descripción: ${doc.data().description} </p>
+                                       <p> Maestro: ${doc.data().teacher} </p>
+                                       <p> Duración: ${doc.data().duration} </p>
+                                       <button onClick="deleteCourse(this)"> Eliminar este curso </button> 
+                                       <button onClick="update(this)"> Actualizar este curso </button>
+                                       `
+            console.log(doc.data().description)
+        }else{
+            console.log('nop :(')
         }
-    })
+        })
+    
+    console.log(id.id)
 }
+
+function deleteCourse(id){
+    
+    alert('curso eliminado'+id.id)
+
+}
+/*
+FOR STUDENTS .HTML
+*/
+
---
+++ students.js
@@ -0,0 +1,156 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwGli-mryIZch0j0DF0eihCZXiWbGyblE",
+    authDomain: "actosoftpanel.firebaseapp.com",
+    databaseURL: "https://actosoftpanel.firebaseio.com",
+    projectId: "actosoftpanel",
+    storageBucket: "actosoftpanel.appspot.com",
+    messagingSenderId: "477113114096",
+    appId: "1:477113114096:web:292e0794332bd76b6cd135",
+    measurementId: "G-2LG9BXE7LL"
+};
+
+firebase.initializeApp(firebaseConfig);
+//Database start
+const db = firebase.firestore();
+//Storage firebase
+const storage = firebase.storage();
+
+
+const studentForm = document.querySelector("#formStudent")
+const firstName = document.querySelector('#firstName')
+const surName = document.querySelector('#lastName')
+const studentEmail = document.querySelector('#email')
+const studentPhone = document.querySelector('#studentPhone')
+const studentAge = document.querySelector('#studentAge')
+const studentBio = document.querySelector('#studentBio')
+const profilePhoto = document.querySelector('#profilePhoto')
+const buttonStudent = document.querySelector('#sendStudentData')
+const studentContent = document.querySelector('#studentsContent')
+
+document.addEventListener('DOMContentLoaded', function () {
+    db.collection('courses')
+        .orderBy("timestamp", "asc")
+        .onSnapshot(function (querySnapshot) {
+            studentForm.innerHTML = ''
+
+            const courses = []
+            querySnapshot.forEach(function (item) {
+                courses.push({'id':item.id, ...item.data()})
+                
+            })
+            let innerHTML = '<p> Selecciona los cursos del estudiante </p>'
+
+            courses.forEach(function (course) {
+    
+            
+                innerHTML += `
+                
+                <input type="checkbox" id="${course.title}" name="courses" value="${course.title}" class="inputCourses">
+                <label for="${course.title}"> ${course.title}</label>
+                
+              
+              
+                `
+
+
+            })
+            innerHTML += ''
+
+            studentForm.innerHTML += innerHTML;
+
+        })
+
+})
+
+const studentCourses = document.querySelectorAll('input[type="checkbox"]:checked')
+
+
+function uploadToStorage(file, docId) {
+    return new Promise(function (resolve, reject) {
+        const imageRef = storage.ref(`students/images/${docId}.${file.type.split('/')[1]}`)
+        imageRef.put(file)
+            .then(() => {
+                imageRef.getDownloadURL()
+                    .then(function (url) {
+                        resolve(url)
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                        reject(error)
+                    })
+            })
+            .catch((error) => {
+                console.log(error)
+                reject(error)
+            })
+    })
+}
+
+buttonStudent.addEventListener('click', (event) => {
+    event.preventDefault()
+    const image = profilePhoto.files[0]
+    db.collection('students').add({
+        firstName: firstName.value,
+        surname: surName.value,
+        email: studentEmail.value,
+        phone: studentPhone.value,
+        age: studentAge.value,
+        biography: studentBio.value,
+        courses: studentCourses.value,
+        timestamp: firebase.firestore.Timestamp.now()
+    }).then(function (docRef) {
+        uploadToStorage(image, docRef.id)
+            .then(function (url) {
+                db.collection('students').doc(docRef.id).update({
+                    image: url
+                }).then(function () {
+                    console.log('Exitoso')
+                }).catch(function (error) {
+                    console.log('No exitoso')
+                })
+            })
+    }).catch(function (error) {
+        console.log(error)
+        alert('No se pudo guardar el curso')
+    })
+    messagesContainer.innerHTML = ''
+    
+
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    db.collection('students')
+        .orderBy("timestamp", "asc")
+        .onSnapshot(function (querySnapshot) {
+            studentContent.innerHTML = ''
+
+            const students = []
+            querySnapshot.forEach(function (item) {
+                students.push({'id':item.id, ...item.data()})
+                console.log(item)
+            })
+            let innerHTML = ''
+
+            students.forEach(function (student) {
+                
+    
+
+            
+                innerHTML += `
+                    <div> 
+                    <p> ${student.firstName} </p> 
+                    <p> ${student.courses} </p>
+                    </div>
+              
+                `
+
+
+            })
+            innerHTML += ''
+
+            studentContent.innerHTML += innerHTML;
+
+        })
+
+})
